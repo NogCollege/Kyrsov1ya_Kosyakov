a384a1706a5104335fb47fb41d5dc9438b48a674
unknown <vpupkin@company.com>
2024-04-25 14:16:38 +0300

    начало работы
    скачал и установил yii2 и начал работу

---
+++ basic/views/layouts/main.php
@@ -0,0 +1,99 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+use app\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+AppAsset::register($this);
+
+$this->registerCsrfMetaTags();
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+
+<!--<header id="header">
+    <?php
+/*    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Contact', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest
+                ? ['label' => 'Login', 'url' => ['/site/login']]
+                : '<li class="nav-item">'
+                    . Html::beginForm(['/site/logout'])
+                    . Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'nav-link btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>'
+        ]
+    ]);
+    NavBar::end();
+    */?>
+</header>-->
+<header>
+    <div class="nav-menu">
+        <ul class="menu">
+            <div class="logo">
+                <li><h1>Картошка от Антошки</h1></li>
+            </div>
+            <div class="knopki no-vid" >
+                <li>Меню</li>
+                <li>Купоны и Акции</li>
+                <li>Контакты</li>
+                <li>О компании</li>
+            </div>
+            <div class="nomer no-vid">
+                <li>7 (499) 110-20-47</li>
+            </div>
+        </ul>
+    </div>
+</header>
+    <section class="promo">
+        <div class="text container">
+            <h1>Аренда авто <br> в Ногинске</h1>
+            <p>Прокат или аренда автомобиля с водителем и без</p>
+        </div>
+    </section>
+<!--<main id="main" class="flex-shrink-0" role="main">
+    <div class="container">
+        <?php /*if (!empty($this->params['breadcrumbs'])): */?>
+            <?php /*= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) */?>
+        <?php /*endif */?>
+        <?php /*= Alert::widget() */?>
+        <?php /*= $content */?>
+    </div>
+</main>-->
+
+
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
